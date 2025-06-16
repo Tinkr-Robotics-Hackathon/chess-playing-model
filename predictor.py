2e1fa6ed14e93971c9049658a56913180f6ff46d
samthemogul <samuelemeka.guru@gmail.com>
2025-06-16 13:01:42 +0100

    Make stockfish binary executable(2)

--- predictor.py
+++ predictor.py
@@ -1,20 +1,23 @@
-# predictor.py
 import os
-import chess
 import chess.engine
-stock_path = os.path.join(os.path.dirname(__file__), 'engine', 'stockfish-ubuntu-x86-64-avx2')
 
 class ChessMovePredictor:
-    def __init__(self, stockfish_path="engine/stockfish-ubuntu-x86-64-avx2", depth=15):
-        print("Loading Stockfish...")
-        self.engine = chess.engine.SimpleEngine.popen_uci(stock_path)
+    def __init__(self, stockfish_path=None, depth=15):
+        if stockfish_path is None:
+            stockfish_path = os.path.join(
+                os.path.dirname(__file__), 
+                'engine', 
+                'stockfish-ubuntu-x86-64-avx2'
+            )
+        print(f"Loading Stockfish from: {stockfish_path}")
+        self.engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
         self.depth = depth
         print("Stockfish loaded.")
 
     def predict_best_move(self, fen):
         board = chess.Board(fen)
         result = self.engine.analyse(board, chess.engine.Limit(depth=self.depth))
-        return result["pv"][0]  # Best move
+        return result["pv"][0]
 
     def close(self):
         self.engine.quit()
