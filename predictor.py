c3c740da5c92935d8241a30fdfec39b77d32f002
samthemogul <samuelemeka.guru@gmail.com>
2025-06-16 12:56:34 +0100

    Fix stockfish path in ChessMovePredictor initialization for correct engine loading

--- predictor.py
+++ predictor.py
@@ -1,11 +1,13 @@
 # predictor.py
+import os
 import chess
 import chess.engine
+stock_path = os.path.join(os.path.dirname(__file__), 'engine', 'stockfish-ubuntu-x86-64-avx2')
 
 class ChessMovePredictor:
     def __init__(self, stockfish_path="engine/stockfish-ubuntu-x86-64-avx2", depth=15):
         print("Loading Stockfish...")
-        self.engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
+        self.engine = chess.engine.SimpleEngine.popen_uci(stock_path)
         self.depth = depth
         print("Stockfish loaded.")
 
